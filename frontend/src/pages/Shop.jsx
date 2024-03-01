134cc96aab187d1abc84cc4d897e4fa5cad499b2
bralao <bernralao@gmail.com>
2024-03-01 16:28:35 +0000

    folders and files and comments added for setup

---
+++ frontend/src/pages/Shop.jsx
@@ -0,0 +1,2 @@
+// Home page
+// render Banner, Popular, About
---
+++ frontend/src/pages/ShopCategory.jsx
@@ -0,0 +1,4 @@
+// show all products, depending on category
+// navbar has surfboard dropdown that has the categories
+// when a category is clicked, it will show all products in that category
+// categories: soft-top, retro, shortboard, mid-length, longboard
