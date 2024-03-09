db1553077fe741737e45756b3065bc68e6266102
bralao <bernralao@gmail.com>
2024-03-09 09:14:42 +0000

    shop and category link added to breadcrum

--- frontend/src/components/breadcrum/Breadcrum.jsx
+++ frontend/src/components/breadcrum/Breadcrum.jsx
@@ -2,14 +2,20 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import './Breadcrum.css'
+import { Link } from 'react-router-dom';
 
 const Breadcrum = (props) => {
 
   const { surfboard } = props;
 
+
+
   return (
     <div className="breadcrum">
-      Shop <FontAwesomeIcon icon={faChevronRight} /> {surfboard.category} <FontAwesomeIcon icon={faChevronRight} /> {surfboard.brand} {surfboard.model} {surfboard.height} {surfboard.volume}
+      <Link to='/' style={{ textDecoration: 'none' }}>Shop</Link>
+      <FontAwesomeIcon icon={faChevronRight} />
+      <Link to={`/${surfboard.category}`} style={{ textDecoration: 'none' }}>{surfboard.category}</Link>
+      <FontAwesomeIcon icon={faChevronRight} /> {surfboard.brand} {surfboard.model} {surfboard.height} {surfboard.volume}
     </div>
   )
 }
