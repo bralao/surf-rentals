f7f05188248b115b8256e7da0c0e3d75846ed496
bralao <bernralao@gmail.com>
2024-03-13 15:01:36 +0000

    brands page done, added in navbar and routes in app, and css for it too

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/navbar/Navbar.jsx'
 import Shop from './pages/ShopPage.jsx'
 import CategoryPage from './pages/CategoryPage.jsx'
+import BrandsPage from './pages/BrandsPage.jsx'
 import Surfboard from './pages/SurfboardPage.jsx'
 import Cart from './pages/CartPage.jsx'
 import LoginSignup from './pages/LoginSignupPage.jsx'
@@ -20,6 +21,20 @@ const App = () => {
           <Route path="/longboards" element={<CategoryPage category="longboards"/>}/>
 
           <Route path="/:category/:surfboardId" element={<Surfboard/>} />
+
+          <Route path="/thunderbolt" element={<BrandsPage brand="Thunderbolt" />} />
+          <Route path="/pyzel" element={<BrandsPage brand="Pyzel" />} />
+          <Route path="/channel-islands" element={<BrandsPage brand="Channel Islands" />} />
+          <Route path="/firewire" element={<BrandsPage brand="Firewire" />} />
+          <Route path="/haydenshapes" element={<BrandsPage brand="HaydenShapes" />} />
+          <Route path="/js-industries" element={<BrandsPage brand="JS Industries" />} />
+          <Route path="/sharpeye" element={<BrandsPage brand="Sharpeye" />} />
+          <Route path="/torq" element={<BrandsPage brand="Torq" />} />
+          <Route path="/takayama" element={<BrandsPage brand="Takayama" />} />
+          <Route path="/album" element={<BrandsPage brand="Album" />} />
+          <Route path="/dhd" element={<BrandsPage brand="DHD" />} />
+
+          <Route path="/:category/:surfboardId" element={<Surfboard />} />
 
           <Route path="/login" element={<LoginSignup />} />
           <Route path="/cart" element={<Cart />} />
--- frontend/src/pages/BrandsPage.jsx
+++ frontend/src/pages/BrandsPage.jsx
@@ -1,9 +1,8 @@
-// CategoryPage.jsx
 import React, { useState, useEffect } from 'react';
-import './styles/CategoryPage.css';
+import './styles/BrandsPage.css';
 import Item from '../components/item/Item';
 
-const CategoryPage = (props) => {
+const BrandsPage = (props) => {
   const [surfboards, setSurfboards] = useState([]);
 
   useEffect(() => {
@@ -14,23 +13,24 @@ const CategoryPage = (props) => {
           throw new Error(`Failed to fetch surfboard data`);
         }
         const data = await response.json();
-        const categoryItems = data.filter(item => item.category === props.category)
-        setSurfboards(categoryItems);
+        const brandItems = data.filter(item => item.brand === props.brand);
+        console.log(brandItems)
+        setSurfboards(brandItems);
       } catch (error) {
         console.error(`Error fetching surfboard data:`, error);
       }
     };
 
     fetchData();
-  }, [props.category]);
+  }, [props.brand]);
 
   return (
-    <div className="shop-category">
-      <h1 style={{ textTransform: 'capitalize' }}>{props.category}</h1>
-      <div className="shopcategory-items">
+    <div className="shop-brand">
+      <h1 style={{ textTransform: 'capitalize' }}>{props.brand}</h1>
+      <div className="shopbrand-items">
         {surfboards.map((item) => (
-          <div className="shopcategory-item" key={item.id}>
-            <Item item={item} pageType="category" />
+          <div className="shopbrand-item" key={item.id}>
+            <Item item={item} pageType="brand" />
           </div>
         ))}
       </div>
@@ -38,4 +38,4 @@ const CategoryPage = (props) => {
   );
 };
 
-export default CategoryPage;
+export default BrandsPage;
